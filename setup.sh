96af916ac2f1d0f5cc2afb3b4205dda16d4c6e37
Fatima zahra Sarbout <fsarbout@e2r7p6.1337.ma>
2021-06-12 21:21:45 +0100

    almost done

--- setup.sh
+++ setup.sh
@@ -7,31 +7,33 @@
 minikube delete
 
 #start a cluster using the virtualbox driver
- minikube start --driver=virtualbox --memory=4096
+minikube start --driver=virtualbox --memory=4096
 
 eval $(minikube docker-env)
-
 
 #metallb
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.0/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.0/manifests/metallb.yaml
 
 docker build -t nginx ./srcs/nginx
-# docker build -t wordpress ./srcs/wordpress
-# docker build -t mysql ./srcs/mysql
-# docker build -t phpmyadmin ./srcs/phpmyadmin
+docker build -t wordpress ./srcs/wordpress
+docker build -t mysql ./srcs/mysql
+docker build -t phpmyadmin ./srcs/phpmyadmin
 # docker build -t ftps ./srcs/ftps
 docker build -t influxdb ./srcs/influxdb
 docker build -t grafana ./srcs/grafana
 
 kubectl apply -f ./srcs/metallb_conf.yaml
-# kubectl apply -f ./srcs/mysql/mysql.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
-# kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
-# kubectl apply -f ./srcs/wordpress/wordpress.yaml
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
 # kubectl apply -f ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/grafana/grafana.yaml
 kubectl apply -f ./srcs/influxdb/influxdb.yaml
+
+#copy database from grafana dashboards container to local folders
+# kubectl cp grafana-d4f77759-b2cn2:grafana-7.4.0/data/grafana.db grafana.db
 
 minikube dashboard &
 
---
+++ srcs/ftps/src/start.sh
@@ -0,0 +1,2 @@
+./telegraf-1.17.0/usr/bin/telegraf --config /telegraf-1.17.0/usr/bin/telegraf.conf &
+vsftpd /etc/vsftpd/vsftpd.conf
