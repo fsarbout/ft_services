0106f7f38ce761b32a381bf9e0ab00c09ef0d167
Fatima zahra Sarbout <fsarbout@e2r7p6.1337.ma>
2021-06-14 21:44:24 +0100

    done

--- srcs/ftps/src/start.sh
+++ srcs/ftps/src/start.sh
@@ -1,5 +1,4 @@
 rc-status
 touch /run/openrc/softlevel
 rc-service telegraf start
-# ./telegraf-1.17.0/usr/bin/telegraf --config /telegraf-1.17.0/usr/bin/telegraf.conf &
 vsftpd /etc/vsftpd/vsftpd.conf
